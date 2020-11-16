0b464b20244bb97e1957a8fde51fa2a25120a160
Disso <idrissojr@gmail.com>
2020-11-16 09:03:52 +0200

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,8 +8,8 @@
     <p class="mb-16">ut labore et dolore magna aliqua.</p>
     <div class="flex flex-wrap">
       <div class="w-full md:w-1/3 my-8 md:my-0 bg-lightgray">
-        <div class="relative mx-20 sm:mx-32 md:mx-0">
-          <img class="w-full m-auto" src="../assets/about1.png" alt="person1" />
+        <div class="relative inline-block mx-8 sm:mx-20 md:mx-0">
+          <img class="h-full" src="../assets/about1.png" alt="person1" />
           <div class="absolute bottom-0 left-0 bg-yellow-400 py-3 px-4 w-3/4">
             <h3 class="text-lx font-bold">Luke Skywalker</h3>
             <p class="text-white mt-2">Web Designer</p>
@@ -17,8 +17,8 @@
         </div>
       </div>
       <div class="w-full md:w-1/3 my-8 md:my-0 bg-lightgray">
-        <div class="relative mx-20 sm:mx-32 md:mx-0">
-          <img class="w-full m-auto" src="../assets/about2.png" alt="person2" />
+        <div class="relative inline-block mx-8 sm:mx-20 md:mx-0">
+          <img class="h-full" src="../assets/about2.png" alt="person2" />
           <div class="absolute bottom-0 left-0 bg-yellow-400 py-3 px-4 w-3/4">
             <h3 class="text-lx font-bold">Han Solo</h3>
             <p class="text-white mt-2">Graphic Designer</p>

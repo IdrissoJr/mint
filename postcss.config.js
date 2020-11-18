b1e286582850dd90f782e9ae33ac77f03c8bccdc
Disso <idrissojr@gmail.com>
2020-11-18 16:50:25 +0200

    Add files via upload

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,12 @@
-const IN_PRODUCTION = process.env.NODE_ENV === "production";
-
 module.exports = {
   plugins: [
-    IN_PRODUCTION &&
+    require("tailwindcss"),
+    require("autoprefixer"),
+    // purge css
+    process.env.NODE_ENV === "production" &&
       require("@fullhuman/postcss-purgecss")({
         content: [`./public/**/*.html`, `./src/**/*.vue`],
-        defaultExtractor(content) {
-          const contentWithoutStyleBlocks = content.replace(
-            /<style[^]+?<\/style>/gi,
-            ""
-          );
-          return (
-            contentWithoutStyleBlocks.match(
-              /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
-            ) || []
-          );
-        },
-        safelist: [
-          /-(leave|enter|appear)(|-(to|from|active))$/,
-          /^(?!(|.*?:)cursor-move).+-move$/,
-          /^router-link(|-exact)-active$/,
-          /data-v-.*/
-        ]
-      })
-  ]
+        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
+  ],
 };
